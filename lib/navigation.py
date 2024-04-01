352db972e0afa83ed9cfaf165de4e7b82b9970fd
sammax <dsammax@outlook.com>
2024-04-01 11:01:08 +0200

    update navigation.py

--- lib/navigation.py
+++ lib/navigation.py
@@ -1,5 +1,6 @@
 import logging
 import os
+from threading import Thread
 import time
 
 import requests
@@ -32,7 +33,6 @@ from lib.settings import (
     get_files_order,
     get_metadata_timeout,
 )
-from lib.torrest.api import TorrestError
 from lib.utils import sizeof_fmt
 
 set_logger()
@@ -47,12 +47,13 @@ class PlayError(Exception):
 
 
 class CanceledError(PlayError):
-    def __init__(self, e, info_hash):
+    def __init__(self, e, info_hash, name):
         super(CanceledError, self).__init__(e)
         self._info_hash = info_hash
+        self._name = name
 
     def handle(self):
-        handle_player_stop(self._info_hash)
+        handle_player_stop(self._info_hash, self._name)
 
 
 def check_playable(func):
@@ -122,37 +123,39 @@ def query_arg(name, required=True):
 
 
 def get_state_string(state):
-    if state == 1:
-        return 'Torrent getting info'
+    if state == 0:
+        return "Torrent added"
+    elif state == 1:
+        return "Torrent getting info"
+    elif state == 2:
+        return "Torrent preload"
     elif state == 3:
-        return 'Torrent working'
+        return "Torrent working"
+    elif state == 4:
+        return "Torrent closed"
     elif state == 5:
-        return 'Torrent in db'
+        return "Torrent in db"
     else:
         return translate(30230)
 
 
 def get_status_labels(info_hash):
-    status = api.get_torrent_info(info_hash)
+    info = api.get_torrent_info(info_hash)
     return (
-        "{:s}".format(get_state_string(status.get("stat"))),
-        "S:{:d} P:{:d}/{:d}".format(
-            status.get("connected_seeders"),
-            status.get("active_peers"),
-            status.get("total_peers"),
+        "{:s}".format(get_state_string(info.get("stat"))),
+        "D:{:s}/s U:{:s}/s S:{:d} P:{:d}/{:d}".format(
+            sizeof_fmt(info.get("download_speed")), 
+            sizeof_fmt(info.get("upload_speed")),
+            info.get("connected_seeders"),
+            info.get("active_peers"),
+            info.get("total_peers"),
         ),
     )
 
 
-def handle_player_stop(info_hash, name=None, initial_delay=0.5, listing_timeout=10):
+def handle_player_stop(info_hash, name, initial_delay=0.5, listing_timeout=10):
     if not ask_to_delete_torrent():
         return
-    try:
-        info = api.torrent_info(info_hash)
-    except TorrestError:
-        return
-    if name is None:
-        name = info.name
 
     sleep(int(initial_delay * 1000))
     start_time = time.time()
@@ -164,7 +167,7 @@ def handle_player_stop(info_hash, name=None, initial_delay=0.5, listing_timeout=
 
     remove_torrent = Dialog().yesno(ADDON_NAME, name + "\n" + translate(30241))
     if remove_torrent:
-        api.remove_torrent(info_hash, delete=True)
+        api.remove_torrent(info_hash)
         current_folder = getInfoLabel("Container.FolderPath")
         if current_folder == plugin.url_for(torrent_files, info_hash):
             executebuiltin("Action(Back)")
@@ -199,9 +202,13 @@ def torrents():
             (translate(30235), media(play_info_hash, info_hash=info_hash))
         ]
 
+        if torrent.get("stat") in [2, 3]:
+            context_menu_items.append(
+                (translate(30208), action(torrent_action, info_hash, "stop"))
+            )
+
         context_menu_items.extend(
             [
-                (translate(30211), action(torrent_action, info_hash, "pause")),
                 (
                     translate(30242),
                     action(torrent_action, info_hash, "remove_torrent"),
@@ -228,10 +235,7 @@ def torrent_action(info_hash, action_str):
     needs_refresh = True
 
     if action_str == "stop":
-        api.torrent_drop(info_hash)
-    elif action_str == "resume":
-        # api.resume_torrent(info_hash)
-        pass
+        api.drop_torrent(info_hash)
     elif action_str == "remove_torrent":
         api.remove_torrent(info_hash)
     elif action_str == "torrent_status":
@@ -246,9 +250,9 @@ def torrent_action(info_hash, action_str):
 
 
 def torrent_status(info_hash):
-    status = api.get_torrent_info(info_hash)
+    status = api.get_torrent_info(link=info_hash)
     notification(
-        "{} ({}%)".format(status.get("stat_string"), status.get("")),
+        "{}".format(status.get("stat_string")),
         status.get("name"),
         sound=False,
     )
@@ -266,8 +270,8 @@ def sort_files(files):
 @check_directory
 def torrent_files(info_hash):
     info = api.get_torrent_info(link=info_hash)
-    title = info.get("title")
     file_stats = info.get("file_stats")
+
     for f in file_stats:
         name = f.get("path")
         id = f.get("id")
@@ -276,12 +280,12 @@ def torrent_files(info_hash):
         file_li.setPath(serve_url)
 
         context_menu_items = []
-        info_labels = {"title": title}
+        info_labels = {"title": info.get("title")}
         if is_picture(name):
-            url = plugin.url_for(display_picture, info_hash, id)
+            url = plugin.url_for(display_picture, info_hash, id, name)
             file_li.setInfo("pictures", info_labels)
         elif is_text(name):
-            url = plugin.url_for(display_text, info_hash, id)
+            url = plugin.url_for(display_text, info_hash, id, name)
         else:
             url = serve_url
             if is_video(name):
@@ -293,10 +297,11 @@ def torrent_files(info_hash):
 
             if info_type is not None:
                 kwargs = dict(info_hash=info_hash, file_id=id, path=name)
+
                 url = plugin.url_for(play, **kwargs)
                 file_li.setInfo(info_type, info_labels)
                 file_li.setProperty("IsPlayable", "true")
-                kwargs = dict(info_hash=info_hash, file_id=id)
+
                 context_menu_items.append(
                     (translate(30235), media(buffer_and_play, **kwargs))
                 )
@@ -306,52 +311,55 @@ def torrent_files(info_hash):
         addDirectoryItem(plugin.handle, url, file_li)
 
 
-@plugin.route("/display_picture/<info_hash>/<file_id>")
-def display_picture(info_hash, file_id):
-    show_picture(api.serve_url(info_hash, file_id))
+@plugin.route("/display_picture/<info_hash>/<file_id>/<path>")
+def display_picture(info_hash, file_id, path):
+    show_picture(api.get_stream_url(link=info_hash, path=path, file_id=file_id))
 
 
-@plugin.route("/display_text/<info_hash>/<file_id>")
-def display_text(info_hash, file_id):
-    r = requests.get(api.serve_url(info_hash, file_id))
-    Dialog().textviewer(api.file_info(info_hash, file_id).name, r.text)
+@plugin.route("/display_text/<info_hash>/<file_id>/<path>")
+def display_text(info_hash, file_id, path):
+    r = requests.get(api.get_stream_url(link=info_hash, path=path, file_id=file_id))
+    Dialog().textviewer(path, r.text)
 
 
 @plugin.route("/play_url")
-@check_playable
 @query_arg("url")
+@check_playable
 def play_url(url, buffer=True):
     r = requests.get(url, stream=True)
-    info_hash = api.add_torrent_obj(r.raw, ignore_duplicate=True)
+    info_hash = api.add_torrent_obj(r.raw)
     play_info_hash(info_hash, buffer=buffer)
 
 
 @plugin.route("/play_magnet")
-@check_playable
 @query_arg("magnet")
+@check_playable
 def play_magnet(magnet, buffer=True):
-    info_hash = api.add_magnet(magnet, ignore_duplicate=True)
+    info_hash = api.add_magnet(magnet)
     play_info_hash(info_hash, buffer=buffer)
 
 
 @plugin.route("/play_path")
-@check_playable
 @query_arg("path")
+@check_playable
 def play_file(path, buffer=True):
-    info_hash = api.add_torrent(path, ignore_duplicate=True)
+    info_hash = api.add_torrent(path)
     play_info_hash(info_hash, buffer=buffer)
 
 
 @plugin.route("/play_info_hash/<info_hash>")
 @check_playable
 def play_info_hash(info_hash, buffer=True):
-    if not api.torrent_status(info_hash).has_metadata:
+    info = api.get_torrent_info(info_hash)
+    if info.get("stat") == 1:
         wait_for_metadata(info_hash)
 
-    files = api.files(info_hash, status=False)
+    files = info.get("file_stats")
     min_candidate_size = get_min_candidate_size() * 1024 * 1024
     candidate_files = [
-        f for f in files if is_video(f.path) and f.length >= min_candidate_size
+        f
+        for f in files
+        if is_video(f.get("path")) and f.get("length") >= min_candidate_size
     ]
     if not candidate_files:
         notification(translate(30239))
@@ -361,16 +369,16 @@ def play_info_hash(info_hash, buffer=True):
     else:
         sort_files(candidate_files)
         chosen_index = Dialog().select(
-            translate(30240), [f.name for f in candidate_files]
+            translate(30240), [f.get("path") for f in candidate_files]
         )
         if chosen_index < 0:
             raise PlayError("User canceled dialog select")
         chosen_file = candidate_files[chosen_index]
 
     if buffer:
-        buffer_and_play(info_hash, chosen_file.id)
+        buffer_and_play(info_hash, chosen_file.get("id"), path=chosen_file.get("path"))
     else:
-        play(info_hash, chosen_file.id)
+        play(info_hash, chosen_file.get("id"), path=chosen_file.get("path"))
 
 
 def wait_for_metadata(info_hash):
@@ -383,7 +391,7 @@ def wait_for_metadata(info_hash):
     progress.create(ADDON_NAME, translate(30237))
 
     try:
-        while not api.torrent_status(info_hash).has_metadata:
+        while api.get_torrent_info(info_hash).get("stat") == 1:
             if monitor.waitForAbort(0.5):
                 raise PlayError("Abort requested")
             passed_time = time.time() - start_time
@@ -402,20 +410,24 @@ def wait_for_metadata(info_hash):
 
 
 @plugin.route("/buffer_and_play/<info_hash>/<file_id>")
+@query_arg("path")
 @check_playable
-def buffer_and_play(info_hash, file_id):
-    api.download_file(info_hash, file_id, buffer=True)
-    if api.file_status(info_hash, file_id).buffering_progress < 100:
-        wait_for_buffering_completion(info_hash, file_id)
-    play(info_hash, file_id)
+def buffer_and_play(info_hash, file_id, path):
+    preload_torrent(info_hash, file_id)
+    wait_for_buffering_completion(info_hash, file_id)
+    play(info_hash, file_id, path=path)
+
+
+def preload_torrent(info_hash, file_id):
+    thread = Thread(target=api.preload_torrent, args=(info_hash, file_id))
+    thread.start()
 
 
 def wait_for_buffering_completion(info_hash, file_id):
     close_busy_dialog()
-    info = api.file_info(info_hash, file_id)
+    info = api.get_torrent_file_info(info_hash, file_id)
     of = translate(30244)
     timeout = get_buffering_timeout()
-    last_time = last_done = 0
     start_time = time.time()
 
     monitor = Monitor()
@@ -425,39 +437,49 @@ def wait_for_buffering_completion(info_hash, file_id):
     try:
         while True:
             current_time = time.time()
-            status = api.file_status(info_hash, file_id)
-            if status.buffering_progress >= 100:
+            status = api.get_torrent_file_info(info_hash, file_id)
+            preloaded_bytes = status.get("preloaded_bytes", 0)
+            preload_size = status.get("preload_size", 0)
+
+            if preload_size == 0 or preloaded_bytes == 0:
+                continue
+
+            if preloaded_bytes >= preload_size:
                 break
 
-            total_done = status.buffering_total * status.buffering_progress / 100
-            speed = float(total_done - last_done) / (current_time - last_time)
-            last_time = current_time
-            last_done = total_done
+            buffering_progress = preloaded_bytes * 100 / preload_size
+            speed = status.get("download_speed", 0)
+            name = info.get("name")
+
             progress.update(
-                int(status.buffering_progress),
+                int(buffering_progress),
                 "{} - {:.2f}%\n{} {} {} - {}/s\n{}\n".format(
-                    get_state_string(status.state),
-                    status.buffering_progress,
-                    sizeof_fmt(total_done),
+                    get_state_string(status.get("stat")),
+                    buffering_progress,
+                    sizeof_fmt(preloaded_bytes),
                     of,
-                    sizeof_fmt(status.buffering_total),
+                    sizeof_fmt(preload_size),
                     sizeof_fmt(speed),
-                    info.name,
+                    name,
                 ),
             )
 
             if progress.iscanceled():
-                raise CanceledError("User canceled buffering", info_hash)
+                api.drop_torrent(info_hash)
+                raise CanceledError("User canceled buffering", info_hash, name)
             if 0 < timeout < current_time - start_time:
                 notification(translate(30236))
+                api.drop_torrent(info_hash)
                 raise PlayError("Buffering timeout reached")
             if monitor.waitForAbort(1):
+                api.drop_torrent(info_hash)
                 raise PlayError("Abort requested")
     finally:
         progress.close()
 
 
-@plugin.route("/play/<info_hash>/<file_id>/<path>")
+@plugin.route("/play/<info_hash>/<file_id>")
+@query_arg("path")
 @check_playable
 def play(info_hash, file_id, path):
     name = path
@@ -471,21 +493,8 @@ def play(info_hash, file_id, path):
             if show_status_overlay()
             else None
         ),
-        on_close_handler=lambda: None,
-        #on_close_handler=lambda: handle_player_stop(info_hash, name=path),
+        on_close_handler=lambda: handle_player_stop(info_hash, name=path),
     ).handle_events()
-
-
-@plugin.route("/torrents/<info_hash>/files/<file_id>/<action_str>")
-def file_action(info_hash, file_id, action_str):
-    if action_str == "download":
-        api.download_file(info_hash, file_id)
-    elif action_str == "stop":
-        api.stop_file(info_hash, file_id)
-    else:
-        logging.error("Unknown action '%s'", action_str)
-        return
-    refresh()
 
 
 @plugin.route("/insert")
